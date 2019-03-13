cef8b02380bff25fe2580355788c41257c3ab991
double Wei <614401332@qq.com>
2019-03-13 11:17:56 +0800

    init

---
+++ config/router.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Router = require('koa-router')
+const User = require('../app/controllers/user')
+const App = require('../app/controllers/app')
+
+module.exports = function(){
+	var router = new Router({
+    prefix: '/API'
+  })
+
+  // user
+  router.post('/u/signup', App.hasBody, User.signup)
+  router.post('/u/login', App.hasBody, User.login)
+  router.get('/u/current', User.current)
+  router.post('/u/update', App.hasBody, App.hasToken, User.update)
+
+  // DB Interface test
+  router.get('/test/user/users',User.users)
+  router.post('/test/user/add',User.addUser)
+  router.post('/test/user/delete',User.deleteUser)
+
+  return router
+}
